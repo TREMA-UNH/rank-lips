e879b4bc75c2c97683b18e32de09c5af9ef43d06
Laura Dietz <dietz@cs.umass.edu>
2020-06-21 15:38:57 -0400

    Suggestion to include --is-v10-model on serialization issues

--- src/RankLipsTypes.hs
+++ src/RankLipsTypes.hs
@@ -171,58 +171,13 @@ data FeatureParams = FeatureParams { featureRunsDirectory :: FilePath
                                    }
     deriving (Eq, Show)
 
--- -- | Old-style 'Read'-based JSON representation for feature names.
--- newtype OldFeatureName = OldFeatureName { unOldFeatureName :: Feat }
---   deriving (Eq, Ord, Show)
-
--- instance FromJSON OldFeatureName where
---     parseJSON (Aeson.String str) =
---         let str' = T.drop 1 $ T.dropEnd 1 str
---         in OldFeatureName . Feat <$> parseFeatName str'
-
---     parseJSON x = fail $ "Can only parse FeatName from string values, received "<> show x
-
--- instance FromJSONKey OldFeatureName where
---     fromJSONKey = Aeson.FromJSONKeyTextParser fun
---     -- fromJSONKey = Aeson.FromJSONKeyText fun
---         where fun :: T.Text -> Aeson.Parser OldFeatureName 
---               fun str =
---                 let str' = T.drop 1 $ T.dropEnd 1 str
---                 in OldFeatureName . Feat <$> parseFeatName str'
-
--- newtype CompatModel = CompatModel (SomeModel Feat)
-
--- instance FromJSON CompatModel where
---     parseJSON v = newStyle <|> oldStyle
---       where
---         newStyle = fmap CompatModel (Aeson.parseJSON v)
---         oldStyle = fmap (CompatModel . fixFeatureNames) (Aeson.parseJSON v)
-
---         fixFeatureNames :: SomeModel OldFeatureName -> SomeModel Feat
---         fixFeatureNames (SomeModel model) =
---             case F.mapFeatures fspace (Just . unOldFeatureName) of
---                 F.FeatureMapping fspace' proj ->
---                     let weights' = proj (getWeightVec $ modelWeights' model)
---                     in SomeModel (Model (WeightVec weights'))
---             where
---                 fspace = F.featureSpace $ getWeightVec $ modelWeights' model
-
-
--- loadRankLipsV10Model :: FilePath -> IO (RankLipsModelSerialized Feat)
--- loadRankLipsV10Model modelFile = do
---     modelOrErr <- Aeson.eitherDecode @(CompatModel)  <$> BSL.readFile modelFile 
---     case modelOrErr of
---             Right (CompatModel (model :: SomeModel Feat)) -> return $ RankLipsModelSerialized { rankLipsTrainedModel = model, rankLipsMetaData=[] }
---             Left msg -> fail $ "Issue deserializing v1.0 model from file "<> modelFile<> " because: "<> msg <> "."
-    
-
 
 loadRankLipsModel ::  forall f. (FromJSONKey f, Ord f, Show f) => FilePath -> IO (RankLipsModelSerialized f)
 loadRankLipsModel modelFile = do
     modelOrErr <- Aeson.eitherDecode @(RankLipsModelSerialized f)  <$> BSL.readFile modelFile 
     case modelOrErr of
             Right model -> return model
-            Left msg -> fail $ "Issue deserializing v1.1 model from file "<> modelFile<> " because: "<> msg <> "."
+            Left msg -> fail $ "Issue deserializing v1.1 model from file "<> modelFile<> " because: "<> msg <> ". To load a v1.0 model, please include flag --is-v10-model"
 
 
 
