0b14560ee2b468b4d6972e27800e923980304f19
Laura Dietz <dietz@cs.umass.edu>
2020-06-24 15:34:21 -0400

    change jsonl example to create features from scratch

--- rank-lips-jsonl-example/run-jsonl.sh
+++ rank-lips-jsonl-example/run-jsonl.sh
@@ -3,8 +3,12 @@
 set -x
 set -e
 
-$bin/rank-lips convert-features -d ../rank-lips-example/train-features FeatureA FeatureB FeatureC -o train-features
 
-$bin/rank-lips train -d "./train-features/" -q "train.qrel" -e "x" -O "out"  -o "x"
+$bin/rank-lips convert-features -d ../rank-lips-example/train-features FeatureA FeatureB FeatureC -o train-features
+$bin/rank-lips convert-features -d ../rank-lips-example/test-features FeatureA FeatureB FeatureC -o test-features
+
+$bin/rank-lips train -d "./train-features/" -q "train.qrel" -e "x" -O "out"  -o "x" --jsonl-run
 
 cat out/x-run-train.run 
+
+$bin/rank-lips predict -d "./test-features/" -q "test.qrel" -O "out"  -o "y" -m "./out/x-model-train.json" --jsonl-run
